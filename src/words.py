a8046b39ed00f02a7248d01eab2daf9f0c3a0499
KarimMaoui <karim.maoui@edu.em-lyon.com>
2025-12-05 00:17:07 +0100

    Create words.py

---
+++ src/words.py
@@ -0,0 +1,12 @@
+WORD_PAIRS = [
+    ("Coca Cola", "Pepsi"),
+    ("Guitare", "Violon"),
+    ("Bleu", "Violet"),
+    ("Google", "ChatGPT"),
+    ("Paris", "Londres"),
+    ("Chien", "Loup"),
+    ("Café", "Thé"),
+    ("Plage", "Piscine"),
+    ("Cinéma", "Netflix"),
+    ("Pizza", "Burger")
+]
