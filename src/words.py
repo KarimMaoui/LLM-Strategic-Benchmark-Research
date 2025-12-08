26300ec66a6d3c07bc349c53bf3d7fe77b40c633
KarimMaoui <karim.maoui@edu.em-lyon.com>
2025-12-08 19:21:43 +0100

    Update words.py

--- src/words.py
+++ src/words.py
@@ -1,25 +1,25 @@
 WORD_PAIRS = [
      #--- CEUX QU'ON GARDE (Les concepts forts) ---
-    ("Amour", "Amitié"),           Piège : L'intensité / Romantisme
-    ("Mensonge", "Secret"),        Piège : L'intention (Tromper vs Protéger)
-    ("Génie", "Talent"),           Piège : L'origine (Inné vs Travaillé)
-    ("Miroir", "Fenêtre"),         Piège : La transparence (Reflet vs Vue)
-    ("Prison", "Zoo"),             Piège : Les détenus (Humains vs Animaux)
-    ("Abeille", "Guêpe"),          Piège : L'utilité (Miel vs Nuisible)
-    ("Vampire", "Zombie"),         Piège : L'allure (Classe vs Pourri)
-    ("Ascenseur", "Escalier"),     Piège : L'effort (Mécanique vs Jambes)
+    ("Amour", "Amitié"),           #Piège : L'intensité / Romantisme
+    ("Mensonge", "Secret"),        #Piège : L'intention (Tromper vs Protéger)
+    ("Génie", "Talent"),           #Piège : L'origine (Inné vs Travaillé)
+    ("Miroir", "Fenêtre"),         #Piège : La transparence (Reflet vs Vue)
+    ("Prison", "Zoo"),             #Piège : Les détenus (Humains vs Animaux)
+    ("Abeille", "Guêpe"),          #Piège : L'utilité (Miel vs Nuisible)
+    ("Vampire", "Zombie"),         #Piège : L'allure (Classe vs Pourri)
+    ("Ascenseur", "Escalier"),     #Piège : L'effort (Mécanique vs Jambes)
 
 
     
     
-    ("Médecin", "Vétérinaire"),    Piège : Le patient (Humain vs Animal)
+    ("Médecin", "Vétérinaire"),    #Piège : Le patient (Humain vs Animal)
 
 
-    ("Ski", "Surf"),               Piège : L'élément (Neige/Froid vs Eau/Chaud)
+    ("Ski", "Surf"),               #Piège : L'élément (Neige/Froid vs Eau/Chaud)
 
     
-    ("Hélicoptère", "Avion"),      Piège : Le décollage (Vertical vs Piste)
+    ("Hélicoptère", "Avion"),      #Piège : Le décollage (Vertical vs Piste)
 
 
-    ("Crayon", "Stylo")            Piège : L'effaçable (Gomme vs Permanent)
+    ("Crayon", "Stylo")            #Piège : L'effaçable (Gomme vs Permanent)
 ]
