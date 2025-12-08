6aad39670815a31807888322db73223862afb565
KarimMaoui <karim.maoui@edu.em-lyon.com>
2025-12-08 01:36:13 +0100

    Update words.py

--- src/words.py
+++ src/words.py
@@ -1,27 +1,27 @@
-# src/words.py
+ src/words.py
 
 WORD_PAIRS = [
-    # --- CEUX QU'ON GARDE (Les concepts forts) ---
-    ("Amour", "Amitié"),          # Piège : L'intensité / Romantisme
-    #("Mensonge", "Secret"),       # Piège : L'intention (Tromper vs Protéger)
-    #("Génie", "Talent"),          # Piège : L'origine (Inné vs Travaillé)
-    #("Miroir", "Fenêtre"),        # Piège : La transparence (Reflet vs Vue)
-    ("Prison", "Zoo"),            # Piège : Les détenus (Humains vs Animaux)
-    #("Abeille", "Guêpe"),         # Piège : L'utilité (Miel vs Nuisible)
-    ("Vampire", "Zombie"),        # Piège : L'allure (Classe vs Pourri)
-    #("Ascenseur", "Escalier"),    # Piège : L'effort (Mécanique vs Jambes)
+     #--- CEUX QU'ON GARDE (Les concepts forts) ---
+    ("Amour", "Amitié"),           Piège : L'intensité / Romantisme
+    ("Mensonge", "Secret"),        Piège : L'intention (Tromper vs Protéger)
+    ("Génie", "Talent"),           Piège : L'origine (Inné vs Travaillé)
+    ("Miroir", "Fenêtre"),         Piège : La transparence (Reflet vs Vue)
+    ("Prison", "Zoo"),             Piège : Les détenus (Humains vs Animaux)
+    ("Abeille", "Guêpe"),          Piège : L'utilité (Miel vs Nuisible)
+    ("Vampire", "Zombie"),         Piège : L'allure (Classe vs Pourri)
+    ("Ascenseur", "Escalier"),     Piège : L'effort (Mécanique vs Jambes)
 
-    # --- LES REMPLAÇANTS (Plus distincts que Sushi/Robot/etc) ---
+
     
-    # Remplace Sushi/Maki (Trop proche) -> Différence d'espèce
-    #("Médecin", "Vétérinaire"),   # Piège : Le patient (Humain vs Animal)
+    
+    ("Médecin", "Vétérinaire"),    Piège : Le patient (Humain vs Animal)
 
-    # Remplace Robot/Cyborg (Trop proche) -> Différence de température
-    #("Ski", "Surf"),              # Piège : L'élément (Neige/Froid vs Eau/Chaud)
 
-    # Remplace Brouillard/Fumée (Trop flou) -> Différence de mécanique
-    #("Hélicoptère", "Avion"),     # Piège : Le décollage (Vertical vs Piste)
+    ("Ski", "Surf"),               Piège : L'élément (Neige/Froid vs Eau/Chaud)
 
-    # Remplace Parapluie/Parasol (Trop proche) -> Différence de persistance
-    #("Crayon", "Stylo")           # Piège : L'effaçable (Gomme vs Permanent)
+    
+    ("Hélicoptère", "Avion"),      Piège : Le décollage (Vertical vs Piste)
+
+
+    ("Crayon", "Stylo")            Piège : L'effaçable (Gomme vs Permanent)
 ]
